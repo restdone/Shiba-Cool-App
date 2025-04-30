1e9af583b082b349237fe0e4a33c765f00810e2f
restdone <42227817+restdone@users.noreply.github.com>
2025-04-30 10:46:03 +0000

    update

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
-### , 
+
 # The functions in this file are required because of a bug in versions of go prior to go1.8.
-#  The bug was reported at ttps://github.com/restdone/Shiba-Cool-App/issues/1 and fixed in
-## https://github.com//restdone/Shiba-Cool-App/daa121167b6ce630aba00195f1c3872cda39a50c.
-##
-## In versions prior to go1.8 the XML encoder did not include pointer fields that were non-nil
-## if the field pointed to a value that was the default value for the pointed to type.
-##
-## To serialize the bool false value when it is set on `VerifyCard`, we must manually control
-## if it is serialized or not.
-#)
+#  The bug was reported at https://github.com/restdone/Shiba-Cool-App/issues/1 and fixed in
+# https://github.com//restdone/Shiba-Cool-App/daa121167b6ce630aba00195f1c3872cda39a50c.
+#
+# In versions prior to go1.8 the XML encoder did not include pointer fields that were non-nil
+# if the field pointed to a value that was the default value for the pointed to type.
+#
+# To serialize the bool false value when it is set on `VerifyCard`, we must manually control
+# if it is serialized or not.
+#
 
 setup(
     name="shiba",
